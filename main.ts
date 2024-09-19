6464242c1dcc2ebd9a242171acb5a19291db74e3
iwanhae <iwanhae@gmail.com>
2024-09-19 15:21:50 +0900

    ollama

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 import "jsr:@std/dotenv/load";
 import { SocketModeClient } from "npm:@slack/socket-mode";
 import { LogLevel } from "npm:@slack/logger";
+import { chat } from "./ollama.ts";
+
+const SYSTEM_PROMPT = Deno.readTextFileSync("./prompt.txt");
 
 interface Message {
     type: string;
@@ -10,20 +13,32 @@ interface Message {
     ts: string;
 }
 
-function handleMessage(message: Message) {
-    // TODO: implement 
+function getEnvOrThrow(key: string): string {
+    const value = Deno.env.get(key);
+    if (!value) {
+        throw new Error(`${key} is not set`);
+    }
+    return value;
+}
+
+const slackAppToken = getEnvOrThrow("SLACK_APP_TOKEN");
+const slackBotToken = getEnvOrThrow("SLACK_BOT_TOKEN");
+
+async function handleMessage(message: Message) {
+    // TODO: reply to the message with translated message
+    // if message is a thread, reply to the thread
+    // if message is not a thread, create a new thread and reply to the thread
+
     console.log(message);
+    const translation = await chat(SYSTEM_PROMPT, message.text);
+    console.log(translation);
 }
 
 if (import.meta.main) {
-    const token = Deno.env.get("SLACK_APP_TOKEN");
-    if (!token) {
-        throw new Error("SLACK_APP_TOKEN is not set");
-    }
     const client = new SocketModeClient({
         logLevel: LogLevel.INFO,
         pingPongLoggingEnabled: true,
-        appToken: token,
+        appToken: slackAppToken,
     });
 
     client.on("message", async ({ ack, event }) => {
--- ollama.ts
+++ ollama.ts
@@ -1,26 +1,22 @@
 import ollama from "npm:ollama";
 
-const OLLAMA_MODEL = "gemma2:latest";
+const OLLAMA_MODEL = "gemma2:9b";
+const TEST_PROMPT = Deno.readTextFileSync("./prompt.txt");
 
 export async function chat(system: string, message: string) {
-    const response = await ollama.chat({
+    const response = await ollama.generate({
         model: OLLAMA_MODEL,
-        messages: [
-            { role: "system", content: system },
-            {
-                role: "user",
-                content: message,
-            },
-        ],
+        system: system,
+        prompt: message,
     });
-    return response.message.content;
+    return response.response;
 }
 
 if (import.meta.main) {
     console.log("Testing ollama chat...");
     const response = await chat(
-        "You are a helpful assistant",
-        "Why is the sky blue?",
+        TEST_PROMPT,
+        "이 번역기 제대로 작동하는건지가 궁금한데요, 이해하기에 무리가 없나요?",
     );
     console.log(response);
 }
